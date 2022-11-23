fa811a4825b03a1adf4983a9675b916192d3f20f
krzm <kmazanek@gmail.com>
2022-11-23 16:04:59 +0100

    refactor

---
+++ Html.Code.Gen.Lib/Tutorial/TutorialsGenerator.cs
@@ -0,0 +1,75 @@
+using Html.Code.Gen.Lib.Serialize;
+
+namespace Html.Code.Gen.Lib;
+
+public class TutorialsGenerator : IHtmlGenerator
+{
+    public void GenerateHtmlFiles()
+    {
+        Console.WriteLine("Generating html files...");
+        ProcessInputFiles();
+    }
+
+    private void ProcessInputFiles()
+    {
+        foreach (var file in ConvertFiles(GetFiles()))
+        {
+            PrintFileData(file);
+            if (IsThereInputFile(file))
+            {
+                PrintNoInputFile(file);
+                continue;
+            }
+            CreateOutputFile(file);
+        }
+    }
+
+    private Dictionary<string, FileDtoRecord> ConvertFiles(Dictionary<string, FileDto> files)
+    {
+        var data = new Dictionary<string, FileDtoRecord>();
+        foreach (var file in files)
+        {
+            data.Add(file.Key, ConvertFile(file.Value));
+        }
+        return data;
+    }
+
+    private FileDtoRecord ConvertFile(FileDto file)
+    {
+        ArgumentNullException.ThrowIfNull(file.JsonPath);
+        ArgumentNullException.ThrowIfNull(file.HtmlPath);
+        return new FileDtoRecord(file.JsonPath, file.HtmlPath);
+    }
+
+    private Dictionary<string, FileDto> GetFiles()
+    {
+        return new Deserizalizer().Deserialize<FileDto>(
+            @"C:\atari-monk\Code\apps-data\html-code-gen\files.json");
+    }
+
+    private void PrintFileData(KeyValuePair<string, FileDtoRecord> file)
+    {
+        Console.WriteLine($"File: {file.Key}");
+        Console.WriteLine($"Input: {file.Value.JsonPath}");
+        Console.WriteLine($"Output: {file.Value.HtmlPath}");
+    }
+
+    private bool IsThereInputFile(KeyValuePair<string, FileDtoRecord> file)
+    {
+        return File.Exists(file.Value.JsonPath) == false;
+    }
+
+    private void PrintNoInputFile(KeyValuePair<string, FileDtoRecord> file)
+    {
+        Console.WriteLine($"Input File: {file.Key} is missing!");
+    }
+
+    private void CreateOutputFile(KeyValuePair<string, FileDtoRecord> file)
+    {
+        File.WriteAllText(
+            file.Value.HtmlPath
+            , new HtmlGen().GetHtml(
+                new TutorialData().Deserialize(
+                    file.Value.JsonPath)));
+    }
+}
--- Html.Code.Gen/Program.cs
+++ Html.Code.Gen/Program.cs
@@ -1,76 +1,7 @@
 ﻿using Html.Code.Gen.Lib;
-using Html.Code.Gen.Lib.Serialize;
 
-GenerateHtmlFiles();
-
-static void GenerateHtmlFiles()
-{
-    Console.WriteLine("Generating html files...");
-    ProcessInputFiles();
-}
-
-static void ProcessInputFiles()
-{
-    foreach (var file in ConvertFiles(GetFiles()))
-    {
-        PrintFileData(file);
-        if (IsThereInputFile(file))
-        {
-            PrintNoInputFile(file);
-            continue;
-        }
-        CreateOutputFile(file);
-    }
-}
-
-static Dictionary<string, FileDtoRecord> ConvertFiles(Dictionary<string, FileDto> files)
-{
-    var data = new Dictionary<string, FileDtoRecord>();
-    foreach (var file in files)
-    {
-        data.Add(file.Key, ConvertFile(file.Value));
-    }
-    return data;
-}
-
-static FileDtoRecord ConvertFile(FileDto file)
-{
-    ArgumentNullException.ThrowIfNull(file.JsonPath);
-    ArgumentNullException.ThrowIfNull(file.HtmlPath);
-    return new FileDtoRecord(file.JsonPath, file.HtmlPath);
-}
-
-static Dictionary<string, FileDto> GetFiles()
-{
-    return new Deserizalizer().Deserialize<FileDto>(
-        @"C:\atari-monk\Code\apps-data\html-code-gen\files.json");
-}
-
-static void PrintFileData(KeyValuePair<string, FileDtoRecord> file)
-{
-    Console.WriteLine($"File: {file.Key}");
-    Console.WriteLine($"Input: {file.Value.JsonPath}");
-    Console.WriteLine($"Output: {file.Value.HtmlPath}");
-}
-
-static bool IsThereInputFile(KeyValuePair<string, FileDtoRecord> file)
-{
-    return File.Exists(file.Value.JsonPath) == false;
-}
-
-static void PrintNoInputFile(KeyValuePair<string, FileDtoRecord> file)
-{
-    Console.WriteLine($"Input File: {file.Key} is missing!");
-}
-
-static void CreateOutputFile(KeyValuePair<string, FileDtoRecord> file)
-{
-    File.WriteAllText(
-        file.Value.HtmlPath
-        , new HtmlGen().GetHtml(
-            new TutorialData().Deserialize(
-                file.Value.JsonPath)));
-}
+IHtmlGenerator generator = new TutorialsGenerator();
+generator.GenerateHtmlFiles();
 
 // var filesScheme = new FilesSerizalizer();
 // filesScheme.SerializeSchema();
